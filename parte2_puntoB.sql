62de721f3191e7b83016ba1e94a2afe672bd7329
polincalin <160639694+polincalin@users.noreply.github.com>
2026-06-02 17:01:05 -0500

    Remove pos_z_raw

--- parte2_puntoB.sql
+++ parte2_puntoB.sql
@@ -1,4 +1,3 @@
-
 -- 1. Crear tabla de Staging
 CREATE TABLE IF NOT EXISTS staging_telemetry (
     game_id_raw      TEXT,
@@ -7,7 +6,6 @@ CREATE TABLE IF NOT EXISTS staging_telemetry (
     tic_raw          TEXT,
     pos_x_raw        TEXT,
     pos_y_raw        TEXT,
-    pos_z_raw        TEXT,
     mom_x_raw        TEXT,
     mom_y_raw        TEXT,
     angle_raw        TEXT,
@@ -34,7 +32,7 @@ WHERE game_id_raw IS NULL OR player_id_raw IS NULL OR tic_raw IS NULL;
 
 INSERT INTO TelemetryEvent (
     game_id, player_id, sector_id, tic, 
-    pos_x, pos_y, pos_z, 
+    pos_x, pos_y, 
     mom_x, mom_y, 
     angle, fov, health, armor, ammo
 )
@@ -45,7 +43,6 @@ SELECT DISTINCT ON (CAST(game_id_raw AS INT), CAST(player_id_raw AS INT), CAST(t
     CAST(tic_raw AS INT),
     CAST(pos_x_raw AS DOUBLE PRECISION),
     CAST(pos_y_raw AS DOUBLE PRECISION),
-    CAST(pos_z_raw AS DOUBLE PRECISION),
     CAST(NULLIF(mom_x_raw, '') AS DOUBLE PRECISION),
     CAST(NULLIF(mom_y_raw, '') AS DOUBLE PRECISION),
     CAST(NULLIF(angle_raw, '') AS DOUBLE PRECISION),
@@ -58,4 +55,3 @@ WHERE game_id_raw IS NOT NULL
   AND player_id_raw IS NOT NULL 
   AND tic_raw IS NOT NULL
 ORDER BY CAST(game_id_raw AS INT), CAST(player_id_raw AS INT), CAST(tic_raw AS INT);
-
